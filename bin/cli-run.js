e1a63e62361015c7205a202755258abc11a42c89
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-11-15 18:49:56 +0100

    Fix seeding process logs

--- bin/cli-run.js
+++ bin/cli-run.js
@@ -5,12 +5,11 @@ const database = require("../lib/database");
 const scripts = require("../lib/scripts");
 
 const _run = async type => {
-  console.log("seeding database...");
+  console.log("running script database...");
   try {
     await scripts.run(database, type);
-    console.log("database seeded");
   } catch (e) {
-    console.log("failed to seed database", e);
+    console.log("failed to run database", e);
   } finally {
     console.log("draining database connections...");
     database.closeAllConnections();
