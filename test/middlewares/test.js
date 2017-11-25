6af011576c7d1a16c1dee59dae0f9f930384ab52
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-11-25 20:19:13 +0100

    Update plugins/middlewares to instantiate reouter optionally and in function

--- test/middlewares/test.js
+++ test/middlewares/test.js
@@ -1,27 +1,29 @@
 const Router = require("express").Router;
 
-const test = new Router();
+module.exports = () => {
+  const test = new Router();
 
-test.get("/test", (req, res, next) => {
-  res.send("hello world");
-});
+  test.get("/test", (req, res, next) => {
+    res.send("hello world");
+  });
 
-test.get("/context-test", (req, res, next) => {
-  req.context
-    .getObjects("Company")
-    .then(data => {
-      res.send(data);
-    })
-    .catch(next);
-});
+  test.get("/context-test", (req, res, next) => {
+    req.context
+      .getObjects("Company")
+      .then(data => {
+        res.send(data);
+      })
+      .catch(next);
+  });
 
-test.get("/me", async (req, res, next) => {
-  try {
-    const payload = await req.jwt.payload();
-    res.send(payload);
-  } catch (e) {
-    next(e);
-  }
-});
+  test.get("/me", async (req, res, next) => {
+    try {
+      const payload = await req.jwt.payload();
+      res.send(payload);
+    } catch (e) {
+      next(e);
+    }
+  });
 
-module.exports = test;
+  return test;
+};
