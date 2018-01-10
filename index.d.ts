d2ca65307a15ec9db1592f56bd86359f23400ac2
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2018-01-10 12:23:46 +0100

    Fix index file to be compatible with declaration, add ts declaration

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import { NappJS as _NappJS } from './lib/napp';
+
+export { NappJSModule } from "./lib/model";
+
+export const NappJS: () => _NappJS;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = config => {
+module.exports.NappJS = config => {
   const NappJS = require("./lib/napp").NappJS;
   return new NappJS();
 };
