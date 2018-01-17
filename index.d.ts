9e1ee34f25769790239d15b5e58de841ca06eb70
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-01-17 11:43:37 +0100

    Fix declaration

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import { NappJS } from './lib/napp';
 
 export { NappJS } from './lib/napp';
-export { NappJSModule } from "./lib/model";
+export { NappJSModule, NappJSService, NappJSScript } from "./lib/model";
 
 export const NewNappJS: () => NappJS;
  
