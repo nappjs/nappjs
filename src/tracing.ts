6d72e0029d29176b2752503227a3cda624e2c1ff
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2019-01-03 12:09:22 +0100

    Update tracing prefix to plain text

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,13 +1,12 @@
 import { existsSync } from 'fs';
 
 const filename = process.cwd() + '/tracing.json';
-const TRACING_PREFIX = process.env.TRACING_PREFIX;
+const TRACING_PREFIX = process.env.TRACING_PREFIX || '';
 
 if (existsSync(filename)) {
   console.log(`starting google-cloud tracing with config ${filename}`);
   require('@google-cloud/trace-agent').start({
-    rootSpanNameOverride: (name: string) =>
-      `${TRACING_PREFIX ? TRACING_PREFIX + ':' : ''}${name}`,
+    rootSpanNameOverride: (name: string) => `${TRACING_PREFIX}${name}`,
     projectId: 'novacloud-cz',
     keyFilename: filename,
     ignoreUrls: ['/healthcheck'],
