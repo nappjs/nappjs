ec3fdb2f14bc1bc7557e8e4fa953ba8b73ed6ef7
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2019-01-24 15:37:13 +0100

    Add TRACING_BUFFER_SIZE

--- src/tracing.ts
+++ src/tracing.ts
@@ -2,6 +2,7 @@ import { existsSync } from 'fs';
 
 const filename = process.cwd() + '/tracing.json';
 const TRACING_PREFIX = process.env.TRACING_PREFIX || '';
+const TRACING_BUFFER_SIZE = process.env.TRACING_BUFFER_SIZE;
 
 if (existsSync(filename)) {
   console.log(`starting google-cloud tracing with config ${filename}`);
@@ -9,6 +10,7 @@ if (existsSync(filename)) {
     rootSpanNameOverride: (name: string) => `${TRACING_PREFIX}${name}`,
     keyFilename: filename,
     ignoreUrls: ['/healthcheck'],
-    ignoreMethods: ['options']
+    ignoreMethods: ['options'],
+    bufferSize: TRACING_BUFFER_SIZE
   });
 }
